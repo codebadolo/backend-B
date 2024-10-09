1b55db3f1f59bd7b7ea70be68d3a520e8ad9d0a6
codebadolo <codewithbadolo@gmail.com>
2024-10-09 14:48:28 +0000

    logs files created   and some couples of trouble fixed

--- core/core/settings/dev.py
+++ core/core/settings/dev.py
@@ -3,7 +3,7 @@ from .base import *
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-
+SECRET_KEY = 'ul!v##s$6a#ud#0%142co4^g7=h!w^ghfyoj%!444i77ydz#ib'
 # Use a simpler database for local development
 DATABASES = {
     'default': {
