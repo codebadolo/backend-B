a0bb0013191fb2da725cac0dd2d78abe12859295
codebadolo <codewithbadolo@gmail.com>
2024-10-10 04:11:26 +0000

    prod fixed

--- core/core/wsgi_prod.py
+++ core/core/wsgi_prod.py
@@ -1,2 +1,8 @@
-wsgi_prod.py
+# core/wsgi_prod.py
 
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings_prod')
+
+application = get_wsgi_application()
