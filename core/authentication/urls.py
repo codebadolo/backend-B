845e6946efc1cc8fab1604d31cbd8ec13fcfa9c3
codebadolo <codewithbadolo@gmail.com>
2024-10-12 20:43:48 +0000

    authentication first trouble fixed

--- core/authentication/urls.py
+++ core/authentication/urls.py
@@ -3,6 +3,7 @@ from .views import UserProfileView  , RegisterView, LoginView, LogoutView , KYCS
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
+    
     path('profile/', UserProfileView.as_view(), name='user-profile'),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
