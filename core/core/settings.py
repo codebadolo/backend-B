964cf35e7ab8a9cc85354bc869d8140a03d3a5c0
codebadolo <codewithbadolo@gmail.com>
2024-10-20 17:56:03 +0000

    corsheaders

--- core/core/settings.py
+++ core/core/settings.py
@@ -36,7 +36,7 @@ DEBUG = True
 INSTALLED_APPS = [
         
    # 'jazzmin',  # Add this at the top of your installed apps
-       
+       'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -62,7 +62,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
 
-     
+        'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -264,3 +264,20 @@ SPECTACULAR_SETTINGS = {
     "custom_js": "custom.js",  # Add your custom JS file if needed
 }
 '''
+
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+
+    "http://75.119.133.13",
+]
+
+CORS_ALLOW_METHODS = [
+    "GET",
+    "POST",
+    "PUT",
+    "PATCH",
+    "DELETE",
+    "OPTIONS"
+]
+CORS_ALLOW_CREDENTIALS = True
